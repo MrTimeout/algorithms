4a53d1de00bb417c0b11988599c8602687246408
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-09-12 23:21:46 +0200

    Commiting program that converts a numerator and denominator to their minus

--- Starting/exercises/1/main.c
+++ Starting/exercises/1/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// typedef struct {
-//     int numerator;
-//     int denominator;
-// } fraction;
+typedef struct {
+    int numerator;
+    int denominator;
+} fraction;
 
 int gcd(int a, int b)
 {
@@ -29,18 +30,38 @@ int gcd(int a, int b)
     return -1;
 }
 
-// int reduce(fraction *f)
-// {
-//     if(f->denominator == 0) //(*f).denominator
-//     {
-//         return -1;
-//     }
-//     return 0;
-// }
+int reduce(fraction *f)
+{
+    if(f->denominator == 0) //(*f).denominator
+    {
+        return -1;
+    }
+    int gcdnd = gcd(f->numerator, f->denominator);
+    if(gcdnd == -1)
+    {
+        return -1;
+    }
+    f->numerator /= gcdnd;
+    f->denominator /= gcdnd;
+    return 0;
+}
+
+char* toString(fraction f)
+{
+    char *str = malloc(20 * sizeof(char));
+    sprintf(str, "%d/%d\n", f.numerator, f.denominator);
+    if(realloc(str, sizeof(str)) == NULL)
+    {
+        return "";
+    }
+    return str;
+}
 
 int main(void)
 {
-    //fraction f = { 20, 25 };
-    printf("%d %d %d\n", 270, 192, gcd(270, 192));
+    fraction f = { 270, 192 };
+    printf("%s", toString(f));
+    reduce(&f);  
+    printf("%s", toString(f));
     return 0;
 }
