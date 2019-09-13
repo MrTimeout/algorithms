8c3725b45eb5b0294d96c3e23027d5b80069db21
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-09-13 21:57:34 +0200

    Adding main of the go programming

---
+++ leetcode/easy/Two-sum/Go/main.go
@@ -0,0 +1,37 @@
+package main
+
+//<-nums
+//for-loop
+//rest
+//if exists
+//return
+//else
+//add to the arr
+
+func main() {
+
+}
+
+func isHere(arr []int, trgt int) int {
+	len := len(arr)
+	for i := 0; i < len; i++ {
+		if arr[i] == trgt {
+			return i
+		}
+	}
+	return -1
+}
+
+func twoSum(nums []int, target int) []int {
+	len := len(nums)
+	tempArr := make([]int, 0, len)
+	var rest int
+	for i := 0; i < len; i++ {
+		rest = target - nums[i]
+		if first := isHere(tempArr, nums[i]); i >= 1 && first != -1 {
+			return []int{first, i}
+		}
+		tempArr = append(tempArr, rest)
+	}
+	return nil
+}
