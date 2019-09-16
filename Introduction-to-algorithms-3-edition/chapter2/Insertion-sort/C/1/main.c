78c8fd5386941b0caded98b3e8ea0838650059c2
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-09-16 16:34:26 +0200

    Adding insertion sort in C

---
+++ Introduction-to-algorithms-3-edition/chapter2/Insertion-sort/C/1/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void insertion_sort(int arr[], int len)
+{
+	int key, i;
+	for(int j = 1; j < len; j++)
+	{
+		key = arr[j];
+		i = j - 1;
+		while(i >= 0 && arr[i] > key)
+		{
+			arr[i+1] = arr[i];
+			i--;
+		}
+		arr[i+1] = key;
+	}
+}
+
+void print_arr(int arr[], int len)
+{
+	for(int i = 0; i < len; i++)
+	{
+		printf("%d\t", *(arr+i));
+	}
+	printf("\n");
+}
+
+int main(void)
+{
+	int arr[] = {5, 2, 4, 6, 1, 3}; 
+	insertion_sort(arr, 6);
+	print_arr(arr,6);
+	return 0;
+}
