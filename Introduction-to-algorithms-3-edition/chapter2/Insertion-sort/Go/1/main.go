3f83caf6db637085b736e25a4d5e96cc8c6803be
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-09-16 11:16:51 +0200

    Adding code on golang programming

---
+++ Introduction-to-algorithms-3-edition/chapter2/Insertion-sort/Go/1/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(insertionSort([]int{5, 2, 4, 6, 1, 3}))
+}
+
+func insertionSort(arr []int) []int {
+	j := 1
+	for j < len(arr) {
+		key := arr[j]
+		i := j - 1
+		for i >= 0 && arr[i] > key {
+			arr[i + 1] = key
+			i--
+		}
+		arr[i + 1] = key
+		j++
+	}
+	return arr
+}
