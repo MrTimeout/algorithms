0c9c599e6d662da281f75a130d62136a69ec7b9f
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-09-17 13:18:39 +0200

    Fixing the method to find out if the number is palindrome or not

--- leetcode/easy/Palindrome_number/C/revert_half_number/main.c
+++ leetcode/easy/Palindrome_number/C/revert_half_number/main.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 
+int testResult()
+{
+    return 2 == 1;
+}
+
 int is_palindrome(int value)
 {
     if(value < 0 || (value % 10 && value != 0))
     {
-        return 1; //false
+        return 0; //false
     }
     int half_revert_number = 0;
-    while(x > half_revert_number)
+    while(value > half_revert_number)
     {
-        half_revert_number = half_revert_number * 10 + x % 10;
-        x /= 10;
+        half_revert_number = half_revert_number * 10 + value % 10;
+        value /= 10;
     }
-    return half_revert_number == x || half_revert_number / 10 == x; //true
+    return half_revert_number == value || half_revert_number / 10 == value; //true
 }
 
 int main(void)
 {
+    printf("result of the test: %d\n", testResult());
     int len = 4;
     int examples[] = { 121, 4554, 4678, 8907 };
-    int results[] = { 0, 0, 1, 1 };
+    int results[] = { 1, 1, 0, 0 };
     for(int i = 0; i < len; i++)
     {
         int r = is_palindrome(examples[i]);
