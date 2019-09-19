ee6998f88123a6d46917c135b1bd5e02fbc1d8e0
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-09-19 14:30:02 +0200

    Adding library

---
+++ Introduction-to-algorithms-3-edition/chapter2/Insertion-sort/C/binary_add/rubbish.h
@@ -0,0 +1,7 @@
+#ifndef _RUBBISH_H_
+#define _RUBBISH_H_
+    extern unsigned int str_len(char * c);
+    extern char* rand_char(int c, int times);
+
+    char* rand_char_internal(int from, int to, int times); 
+#endif
