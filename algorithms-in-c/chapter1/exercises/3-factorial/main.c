2bc52fffc717de79fb284b312cbe1fa591c68989
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-09-20 19:22:37 +0200

    Adding some functions to factorial file

--- algorithms-in-c/chapter1/exercises/3-factorial/main.c
+++ algorithms-in-c/chapter1/exercises/3-factorial/main.c
@@ -1,4 +1,61 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define LONG_FACTORIAL "-long"
+#define INT_FACTORIAL "-int"
+#define TEST "-test"
+
+bool strcomp(a, b)
+const char * a, b;
+{
+    int la = 0, lb = 0;
+    while(*(a++) != '\0')
+    {
+        if(*a != *b)
+            return false;
+        la++;lb++;
+    }
+    return *(const unsigned char*)a - (const unsigned char*)b; 
+}
+
+unsigned int str_len(a)
+char * a;
+{
+    int len = 0;
+    while(*(a++) != '\0')
+        len++;
+    return len;
+}
+
+unsigned int poww(a, b)
+int a, b;
+{
+    if(a == 0)
+        return 0;
+    int t = a;
+    for(int i = 0; i < b; i++)
+    {
+        a *= t;
+    }
+    return a;
+}
+
+bool isNumeric(a, c, n)
+int * a;
+char * c;
+bool n;
+{
+    int len, p;
+    if(!n && *c == '-')
+    {
+        fprintf(stderr, "The \"number\" %s has to be positive\n", c);
+        exit(1);
+    }
+    len = str_len(c);
+    
+    return true;
+}
 
 int greatest(int a, int b)
 {
@@ -41,8 +98,23 @@ int testingfactorial(int from, int to)
 	return 0;
 }
 
-int main(void)
+int main(argc, argv)
+int argc;
+char * argv;
 {
+    bool intfact = true, longfact = false; 
+    while(*(argv++) != '\0')
+    {
+        switch(*argv)
+        {
+            case INT_FACTORIAL:
+               if(*(argv+1))
+               {
+
+               } 
+            break;
+        }
+    }
 	testingfactorial(2, 39);
 	return 0;
 }
