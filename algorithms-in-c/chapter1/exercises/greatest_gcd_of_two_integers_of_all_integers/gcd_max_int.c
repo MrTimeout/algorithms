b9a69019fa07594766a00bf11855f5b91dca66fa
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-09-24 18:13:35 +0200

    Adding unsigned long long xD

--- algorithms-in-c/chapter1/exercises/greatest_gcd_of_two_integers_of_all_integers/gcd_max_int.c
+++ algorithms-in-c/chapter1/exercises/greatest_gcd_of_two_integers_of_all_integers/gcd_max_int.c
@@ -16,6 +16,40 @@ bool isPrime(int n)
     return true;
 }
 
+bool isPrimeLL(unsigned long long int n)
+{
+    unsigned long long int pos = 2;
+    if(n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
+        return false;
+    while(pos < n / 2)
+    {
+        if(n % pos == 0)
+            return false;
+        pos++;
+    }
+    return true;
+}
+
+void fromTheInfinityLL()
+{
+    unsigned long long int a = -1, b = -1;
+    for(unsigned long long int i = ULLONG_MAX; i > 2; i--)
+    {
+        if(isPrimeLL(i))
+        {
+            if(a == -1)
+                a = i;
+            else if (b == -1)
+                b = i;
+            if(a != -1 && b != -1)
+            {
+                fprintf(stdout, "gcd(%lld, %lld) = 1\n", a, b);
+                break;
+            }
+        }
+    }
+}
+
 void fromTheInfinity()
 {
     int a = -1, b = -1;
@@ -39,6 +73,7 @@ void fromTheInfinity()
 int main(void)
 {
     fromTheInfinity();
+    fromTheInfinityLL();
     return 0;
 }
 
