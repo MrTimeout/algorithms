be05c002034221612cbc6ec1fb67a60f21b76917
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-09-24 17:59:23 +0200

    Greatest pair of numbers whos gcd is 1

---
+++ algorithms-in-c/chapter1/exercises/greatest_gcd_of_two_integers_of_all_integers/gcd_max_int.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
+
+bool isPrime(int n)
+{
+    int pos = 2;
+    if(n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
+        return false;
+    while(pos < n / 2)
+    {
+        if(n % pos == 0)
+            return false;
+        pos++;
+    }
+    return true;
+}
+
+void fromTheInfinity()
+{
+    int a = -1, b = -1;
+    for(int i = INT_MAX; i > 2; i--)
+    {
+        if(isPrime(i))
+        {
+            if(a == -1)
+                a = i;
+            else if (b == -1)
+                b = i;
+            if(a != -1 && b != -1)
+            {
+                fprintf(stdout, "gcd(%d, %d) = 1\n", a, b);
+                break;
+            }
+        }
+    }
+}
+
+int main(void)
+{
+    fromTheInfinity();
+    return 0;
+}
+
