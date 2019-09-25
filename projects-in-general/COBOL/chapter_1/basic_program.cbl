dfd66c081472fb5f994eddfcf768eae80dc23a5e
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-09-25 09:00:27 +0200

    Adding COBOL to blow up my mind

---
+++ projects-in-general/COBOL/chapter_1/basic_program.cbl
@@ -0,0 +1,29 @@
+      * this is a comment in COBOL
+      * in COBOL is always necesary use four statments:
+      *
+      * IDENTIFICATION DIVISION.
+      * ---
+      * ENVIRONMENT DIVISION.
+      * ---
+      * DATA DIVISION.
+      * ---
+      * PROCEDURE DIVISION.
+      *
+      * -> IDENTIFICATION DIVISION. is used to identify features of the
+      * program as the name of the programmer, date, etc.
+      *
+      * -> ENVIRONMENT DIVISION. is used to relate the program
+      * to the the computer where is going to be used.
+      * When you write simple programs, the content of the header
+      * can be left in blanck, so, COBOL assume the basics.
+      *
+      * -> DATA DIVISION. data-type to be used  by the program
+      * this could be contained in the WORKING-STORAGE SECTION
+       IDENTIFICATION DIVISION.
+      * BASIC INFORMATION
+       ENVIRONMENT DIVISION.
+      * EMPTY FOR SIMPLE PROGRAMS
+       DATA DIVISION.
+      * EMPTY TOO BECAUSE I DONT KNOW JAJAA
+       PROCEDURE DIVISION.
+      * PROCESSING THE LOGIC
