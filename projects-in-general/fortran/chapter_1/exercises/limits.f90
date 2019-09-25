4497d974412633bda2a21395089e43fe8e2823e9
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-09-25 11:05:33 +0200

    Beyond the limits

---
+++ projects-in-general/fortran/chapter_1/exercises/limits.f90
@@ -0,0 +1,6 @@
+
+program name
+    type (integer(kind=1)) :: uint8
+    type (integer(kind=2)) :: uint16
+    print *, "Max of 8 bits ", huge(uint8), " and max of 16 bits is ", huge(uint16)
+end program name
