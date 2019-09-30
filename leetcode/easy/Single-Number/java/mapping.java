a0be76c2f687e28da4a98e5b18affafed8e23611
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-09-30 16:19:38 +0200

    Adding mapping solution(not tested yet)

---
+++ leetcode/easy/Single-Number/java/mapping.java
@@ -0,0 +1,32 @@
+
+import java.util.HashSet;
+
+class Solution {
+    /*
+     * arr not empty, so we dont need to handle this possible event
+     * arr with only one non repeated value
+     * we loop all the elements in the arr, using a hashmap
+     *
+     */  
+    public int singleNumber(int[] nums)
+    {
+        HashSet<Integer> map = new HashSet<Integer>();
+        for(int x: nums)
+            if(map.contains(x))
+                map.remove(x);
+            else
+                map.add(x);
+        return map.iterator().next();
+    }
+}
+
+public class mapping
+{
+    
+    public static void main(String[] args)
+    {
+        int[] cases = new int[]{2, 2, 1};
+        System.out.println((new Solution()).singleNumber(cases));
+    }
+
+}
