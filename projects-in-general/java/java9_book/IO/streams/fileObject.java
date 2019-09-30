4dd39661d935a15549b1e18c2494c965ae481608
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-09-30 15:39:42 +0200

    Adding some properties and basic knowdlege

--- projects-in-general/java/java9_book/IO/streams/fileObject.java
+++ projects-in-general/java/java9_book/IO/streams/fileObject.java
@@ -4,19 +4,42 @@ import java.io.File;
 public class fileObject
 {
 
-    public static parsingFile(String name)
+    private static java.io.File c = new java.io.File("new.txt");
+
+    public static void parsingFile(java.lang.String name)
     {
         java.io.File f = new java.io.File(name);
         if(f.exists())
         {
             System.out.println(String.format("It can be executed: %b\nIt can be readed: %b\nIt can be wrote: %b\n", f.canExecute(), f.canRead(), f.canWrite()));
+             switch(f.compareTo(c))
+             {
+                case 0:
+                    System.out.println("Exactly the same");
+                break;
+                case 1:
+                    System.out.printf("%s greater than %s\n", toString(f), toString(c));
+                break;    
+                case -1:
+                    System.out.printf("%s less than %s\n", toString(f), toString(c));
+                break;
+             }
         } 
     }
 
     public static void main(String[] args)
     {
         for(String str: args)
-            parsingFiles();
+            parsingFile(str);
+    }
+
+    public static java.lang.String toString(java.io.File f)
+    {
+        return String.format("{\n\t'AbsolutePath': %s,\n\t'Name': %s,\n\t'Parent': %s,\n\t'Path': %s,\n}",
+                             f.getAbsolutePath(),
+                             f.getName(),
+                             f.getParent(),
+                             f.getPath());
     }
 
 }
