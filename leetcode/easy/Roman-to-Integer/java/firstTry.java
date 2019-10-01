d5b8fc0743264dec88c1202607189f5193b4fa3b
estenoesmiputonombre <49493249+estenoesmiputonombre@users.noreply.github.com>
2019-10-01 19:47:59 +0200

    Update firstTry.java

--- leetcode/easy/Roman-to-Integer/java/firstTry.java
+++ leetcode/easy/Roman-to-Integer/java/firstTry.java
@@ -1,82 +1,44 @@
-
-import java.util.HashMap;
-import java.util.Collections;
-
-class Solution
-{
-    /*
-     * From 1 to 3999
-     * MMCM
-     */
-    public int romanToInt(String s)
-    {
-        int result = 0;
+class Solution {
+    public int romanToInt(String str) {
+        int result = 0, len = str.length();
         char c;
-        HashMap<java.lang.Character, java.lang.Integer> m = new HashMap<>();
-        m.put('I', 1);
-        m.put('V', 5);
-        m.put('X', 10);
-        m.put('L', 50);
-        m.put('C', 100);
-        m.put('D', 500);
-        m.put('M', 1000);
-        Collections.sort(m, new Comparator<Integer>()
-                {
-                    public int compare(Integer i, Integer ii)
-                    {
-                        return ii.compareTo(i);
-                    }
-                });
-        for(int i = 0; i < str.length(); i++)
+        for(int i = 0; i < len; i++)
         {
             c = str.charAt(i);
             switch(c)
             {
                 case 'M':
+                    result += 1000;
+                break;
                 case 'D':
-                    result += m.get(c);
+                    result += 500;
                 break;
                 case 'C':
-                    switch(str.charAt(i+1))
-                    {
-                        case 'D':
-                        case 'M':
-                            result -= m.get(c);
-                        break;
-                        default:
-                            result += m.get(c);
-                        break;
-
-                    }
+                    if(i+1 < len && (str.charAt(i+1) == 'M' || str.charAt(i+1) == 'D'))
+                        result -= 100;
+                    else
+                        result += 100;
                 break;
                 case 'L':
-                    result += m.get(c);
+                    result += 50;
                 break;
                 case 'X':
-                    switch(str.charAt(i+1))
-                    {
-                        case 'C':
-                        case 'L':   
-                            result -= m.get(c);   
-                        break;
-                        default:
-                            result += m.get(c);
-                        break;    
-                    }
+                    if(i+1 < len && (str.charAt(i+1) == 'L' || str.charAt(i+1) == 'C'))
+                        result -= 10;
+                    else
+                        result += 10;
                 break;
                 case 'V':
-                    
+                    result += 5;
+                break;
+                case 'I':
+                    if(i+1 < len && (str.charAt(i+1) == 'V' || str.charAt(i+1) == 'X'))
+                        result -= 1;
+                    else
+                        result += 1;
+                break;
             }
         }
-        
-    }
-}
-
-public class FirstTry
-{
-    public static void main(String[] args)
-    {
-        for(String str: args)
-            System.out.println(romanToInt(str));
+        return result;
     }
 }
