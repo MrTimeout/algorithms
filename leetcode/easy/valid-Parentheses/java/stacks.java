fc15bbfa9a6d390c622746a7c2be2b9ae082419a
estenoesmiputonombre <49493249+estenoesmiputonombre@users.noreply.github.com>
2019-10-01 09:59:53 +0200

    Update stacks.java

--- leetcode/easy/valid-Parentheses/java/stacks.java
+++ leetcode/easy/valid-Parentheses/java/stacks.java
@@ -5,16 +5,18 @@ class Solution {
         if(s.length() == 1)
             return false;
         Stack<java.lang.Character> st = new Stack<>();
+        char c;
         st.add(s.charAt(0));
         for(int i = 1; i < s.length(); i++)
         {
-            if(!st.isEmpty() && (s.charAt(i) == st.peek()+1 || s.charAt(i) == st.peek()+2))
+            c = s.charAt(i);
+            if(!st.isEmpty() && (c == st.peek()+1 || c == st.peek()+2))
                 st.pop();
-            else if(!st.isEmpty() && st.peek() == 40 && st.peek() == 132 || st.peek() == 91)
-                st.push((Character) s.charAt(i));
+            else if(c == 40 || c == 123 || c == 91)
+                st.push((Character) c);
             else
                 return false;
         }
-        return true;
+        return st.size() == 0;
     }
 }
