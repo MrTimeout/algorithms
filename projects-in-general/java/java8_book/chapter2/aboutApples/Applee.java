558696537cad5b8a95b3f4998ffd11b90dea33fd
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-10-03 22:54:10 +0200

    Adding some stuff

--- projects-in-general/java/java8_book/chapter2/aboutApples/Applee.java
+++ projects-in-general/java/java8_book/chapter2/aboutApples/Applee.java
@@ -1,6 +1,35 @@
 
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Applee
 {
+
+    public static void fourthAttempt()
+    {
+        System.out.println("Getting all the apples that are green ");
+        printAllInventory(Apple.filterApples(randomApples(10)), new AppleGreenColorPredicate());
+        
+
+    }
+
+    public static java.util.List<Apple> randomApples(int n)
+    {
+        String[] colors = new String[]{ "red", "green", "blue" };
+        java.util.Random rand = new java.util.Random();
+        java.util.List<Apple> r = new java.util.ArrayList<Apple>();
+        for(int i = 0; i < n; i++)
+            r.add(new Apple(rand.nextInt(1000)+1, colors[rand.nextInt(colors.length)]));
+        return r;   
+    }
+
+    public static void printAllInventory(java.util.List<Apple> l)
+    {
+        for(Apple a: l)
+            System.out.println(a);
+    }
+
 
     public static void main(String[] args)
     {
@@ -52,6 +81,15 @@ class Apple
     public int getW()
     {
         return this.w;
+    }
+
+    public static java.util.List<Apple> filterApples(List<Apple> l, ApplePredicate ap)
+    {
+        java.util.List<Apple> r = new ArrayList<>();
+        for(Apple a: l)
+            if(ap.test(a))
+                r.add(a);
+        return r;
     }
 
     @Override
