e6036c44e38e319c8cac0fcde1562effd97c4c7f
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-10-03 13:16:47 +0200

    First, second and third attempt completed

--- projects-in-general/java/java8_book/chapter2/aboutApples/Apple.java
+++ projects-in-general/java/java8_book/chapter2/aboutApples/Apple.java
@@ -15,7 +15,7 @@ public class Apple
         this.weight = w;
     }
 
-    public int getWeigth()
+    public int getWeight()
     {
         return this.weight;
     }
@@ -36,11 +36,42 @@ public class Apple
         return String.format("weight: %3d g, color: %s", this.weight, this.color);
     }
 
+    /**
+     * Imagine that the farmer wants to take also the red apples, what could you do??
+     * see the next attempt below this method.
+     */
     public static java.util.List<Apple> filterGreenApples(java.util.List<Apple> l)
     {
         java.util.List<Apple> r = new java.util.ArrayList<Apple>();
         for(Apple a: l)
             if("green".equals(a.getColor()))
+                r.add(a);
+        return r;
+    }
+
+    public static java.util.List<Apple> filterColorApples(java.util.List<Apple> l, java.lang.String color)
+    {
+        java.util.List<Apple> r = new java.util.ArrayList<Apple>();
+        for(Apple a: l)
+            if(color.equals(a.getColor()))
+                r.add(a);
+        return r;
+    }
+
+    public static java.util.List<Apple> filterWeightApple(java.util.List<Apple> l, int weight)
+    {
+        java.util.List<Apple> r = new java.util.ArrayList<Apple>();
+        for(Apple a: l)
+            if(a.getWeight() > weight)
+                r.add(a);
+        return r;
+    }
+
+    public static java.util.List<Apple> filterApples(java.util.List<Apple> l, java.lang.String color, int weight, boolean flag)
+    {
+        java.util.List<Apple> r = new java.util.ArrayList<Apple>();
+        for(Apple a: l)
+            if((flag && color.equals(a.getColor())) || (!flag && a.getWeight() > weight))
                 r.add(a);
         return r;
     }
@@ -55,11 +86,48 @@ public class Apple
         return r;   
     }
 
-    public static void main(String[] args)
+    public static void printAllInventory(java.util.List<Apple> l)
+    {
+        for(Apple a: l)
+            System.out.println(a);
+    }
+
+    /**
+     * This would be the first attempt, where we can create one method to filter by one color and no more...
+     */
+    public static void firstAttempt()
     {
         java.util.List<Apple> inventory = filterGreenApples(randomApples(10));
         for(Apple a: inventory)
             System.out.println(a);
+    }
+
+    /**
+     * This would be the second attempt, where we can filter by any color that the farmer wants:
+     * Now, imagine that the farmer wants to add other filter like the weight, you could do another method with this filter
+     */
+    public static void secondAttempt()
+    {
+        java.util.List<Apple> greenInventory = filterWeightApple(filterColorApples(randomApples(10), "green"), 150);
+        printAllInventory(greenInventory);
+        java.util.List<Apple> redInventory = filterWeightApple(filterColorApples(randomApples(10), "red"), 150);
+        printAllInventory(redInventory);
+    }
+
+    /**
+     * This would be the third attempt, where we can filter by color or weight using only one method...
+     */
+    public static void thirdAttempt()
+    {
+        java.util.List<Apple> greenInventory = filterApples(randomApples(10), "green", 150, false);
+        printAllInventory(greenInventory);
+        java.util.List<Apple> weightInventory = filterApples(randomApples(10), "green", 150, true);
+        printAllInventory(weightInventory);
+    }
+
+    public static void main(String[] args)
+    {
+        thirdAttempt(); 
     }
 
 }
