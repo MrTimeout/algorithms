a2975527835a025d6d3671e3d7a63aa8954b131a
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-10-06 22:59:30 +0200

    Adding close of the fie and other stuff

--- projects-in-general/java/Docs/IOPackage/InputStreamExamples.java
+++ projects-in-general/java/Docs/IOPackage/InputStreamExamples.java
@@ -4,9 +4,10 @@ public class InputStreamExamples
    
     public static void readingAll() throws java.io.IOException
     {
-        java.io.InputStream input = new java.io.FileInputStream("test.txt");
+        java.io.InputStream input = null;
         try
         {
+            input = new java.io.FileInputStream("test.txt");
             int data;
             while((data=input.read()) != -1)
                 System.out.printf("%c", data);
@@ -14,6 +15,11 @@ public class InputStreamExamples
         catch(java.io.IOException e)
         {
 
+        }
+        finally
+        {
+            if(input != null)
+                input.close();
         }
     }
 
@@ -39,4 +45,10 @@ public class InputStreamExamples
  *      - java.io.FilterInputStream extends java.io.InputStream
  *
  *      - java.io.PushbackInputStream extends java.io.InputStream
+ *
+ *      - java.io.DataInputStream extends java.io.InputStream
+ *
+ *      - java.io.ObjectInputStream extends java.io.InputStream
+ *
+ *      - java.io.SequenceInutStream extends java.io.InputStream
  */ 
