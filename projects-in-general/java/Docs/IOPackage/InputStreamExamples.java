a1404d1fa8889fea1c7b17f6a6c944b13c3d1fb7
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-10-06 23:26:01 +0200

    adding some funny stuff

--- projects-in-general/java/Docs/IOPackage/InputStreamExamples.java
+++ projects-in-general/java/Docs/IOPackage/InputStreamExamples.java
@@ -23,9 +23,68 @@ public class InputStreamExamples
         }
     }
 
+    public static void allMethodsOfInputStream() throws java.io.IOException
+    {
+        java.io.InputStream input = null;
+        int data, amount;
+        byte[] buffer = new byte[50];
+        try
+        {
+            input = new java.io.FileInputStream("test.txt"); 
+
+            System.out.printf("Bytes that are available to read are: %d \n", input.available());
+
+            if(input.markSupported())
+            {
+                input.mark(0);
+                System.out.println("We are marking the first position in the input stream to come back later");
+            }
+            else
+                System.out.println("This java.io.InputStream doesnt support the reset and mark method");
+
+            System.out.println("Reading only the first line: ");
+            amount = 0;
+            while((data = input.read()) != '\n' && data != -1)
+            {
+                amount++;
+                System.out.printf("%c", data);
+            }
+            System.out.println("We reset to the first character of the InputStream");
+            input.reset();
+            
+            System.out.printf("We are skipping the first line, an amount of %d bytes\n", amount);
+            input.skip((long) amount);
+
+            System.out.println("We are going to read the next line because we know the length");
+            input.read(buffer, 0, 5);
+
+            printArr(java.util.Arrays.<Byte>asList(buffer));
+
+            System.out.printf("There are available %d bytes\n", input.available());
+        }
+        catch(java.io.IOException e)
+        {
+
+        }
+        finally
+        {
+            if(input != null)
+                input.close();
+        }
+    }
+
+    public static <T> void printArr(java.util.List<T> arg)
+    {
+        System.out.println("Printing array of things");
+        for(T t: arg)
+            System.out.print(t);
+        System.out.println("");
+    }
+
     public static void main(String... args) throws java.io.IOException
     {
         readingAll();
+        allMethodsOfInputStream();
     }
 
 }
