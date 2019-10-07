95cf9362eaa869945664ba191ba9746325c2d23f
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-10-07 20:41:20 +0200

    Adding more info to IO package

--- projects-in-general/java/Docs/IOPackage/InputStreamExamples.java
+++ projects-in-general/java/Docs/IOPackage/InputStreamExamples.java
@@ -65,6 +65,13 @@ public class InputStreamExamples
             System.out.println("We are going to read the next line because we know the length");
             input.read(buffer, 0, 5);
 
+            Garbage.<Byte>printList(
+                    Garbage.<Byte>toList(
+                        Garbage.toByte(buffer)
+                        ),
+                    (Byte b) -> { byte v; if((v=b.byteValue())!=0) System.out.printf("%c -> %d\n", v, v); }
+                    );
+
             System.out.printf("There are available %d bytes\n", input.available());
         }
         catch(java.io.IOException e)
@@ -97,6 +104,33 @@ public class InputStreamExamples
 interface Consumer<T>
 {
     void accept(T t);
+}
+
+class Garbage
+{
+    
+    public static <T> java.util.List<T> toList(T[] arg)
+    {
+        java.util.List<T> l = new java.util.ArrayList<T>();
+        for(T t: arg)
+            l.add(t);
+        return l;
+    }
+
+    public static <T> void printList(java.util.List<T> list, Consumer<T> c)
+    {
+        for(T t: list)
+            c.accept(t);
+    }
+
+    public static Byte[] toByte(byte[] arr)
+    {
+        java.lang.Byte[] b = new java.lang.Byte[arr.length];
+        for(int i = 0; i < arr.length; i++)
+            b[i] = new java.lang.Byte(arr[i]);
+        return b;
+    }
+
 }
 
 
