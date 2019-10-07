e89dea2b6209d4d92e954887f318e79d7ece361c
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-10-07 21:15:24 +0200

    Adding some practise of collections

---
+++ projects-in-general/java/javaGenerics/chapter2/CollectionsE.java
@@ -0,0 +1,101 @@
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class CollectionsE
+{
+    
+    public static void main(String... args)
+    {
+        Test.testCopy();
+    }
+
+}
+
+class Test
+{
+    public static void testCopy()
+    {
+        List<Integer> ints = Arrays.<Integer>asList(1, 2, 3, 4, 5);
+        List<Integer> intss = new ArrayList<Integer>(Arrays.<Integer>asList(0,0,0,0,0));
+
+        assert ints.size() == intss.size();
+
+        System.out.println("Before the copy method: ");
+        Print.processConsumer(intss, (Integer i) -> System.out.printf("class: %s -> %d\n", i.getClass().getCanonicalName(), i));
+
+        Garbage.copy(intss, ints);
+
+        System.out.println("After the copy method: ");
+        Print.processConsumer(intss, new Consumer<Integer>(){
+            @Override
+            public void accept(Integer i)
+            {
+                System.out.printf("class: %s -> %d\n", i.getClass().getCanonicalName(), i);
+            }
+        });
+
+        intss = Arrays.<Integer>asList(0,0,0,0,0);
+        
+        assert ints.size() == intss.size();
+
+        System.out.println("Before the copyy method: ");
+        Print.processConsumer(intss, (Integer i) -> System.out.printf("class: %s -> %d\n", i.getClass().getCanonicalName(), i));
+
+        Garbage.copyy(intss, ints);
+
+        System.out.println("After the copyy method: ");
+        Print.processConsumer(intss, new Consumer<Integer>(){
+            @Override
+            public void accept(Integer i)
+            {
+                System.out.printf("class: %s -> %d\n", i.getClass().getCanonicalName(), i);
+            }
+        });
+
+    }
+}
+
+interface Consumer<T>
+{
+    void accept(T t);
+}
+
+class Print
+{
+    
+    public static <T> void processConsumer(List<T> list, Consumer<T> c)
+    {
+        for(T t: list)
+            c.accept(t);
+    }
+
+}
+
+class Garbage
+{
+
+    public static <T> void copy(List<T> dst, List<T> src)
+    {
+        if(src.size() <=0 || src.size() > dst.size())
+            throw new java.lang.IllegalArgumentException("Error parsing arguments");
+        ListIterator<T> it = dst.listIterator();
+        for(Iterator<T> i = src.iterator(); i.hasNext();)
+        {
+            it.next();
+            it.set(i.next());
+        }
+    }
+
+    public static <T> void copyy(List<T> dst, List<T> src)
+    {
+        if(src.size() <=0 || src.size() > dst.size())
+            throw new java.lang.IllegalArgumentException("Error parsing arguments");
+        for(int i = 0; i < src.size(); i++)
+            dst.set(i, src.get(i));
+    }
+
+}
