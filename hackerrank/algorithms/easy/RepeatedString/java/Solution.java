8918f91dc8bc8ea89e0bb51b7a8430ac9674fdbf
estenoesmiputonombre <49493249+estenoesmiputonombre@users.noreply.github.com>
2019-10-10 20:16:27 +0200

    Update Solution.java

--- hackerrank/algorithms/easy/RepeatedString/java/Solution.java
+++ hackerrank/algorithms/easy/RepeatedString/java/Solution.java
@@ -2,17 +2,20 @@ public class Solution {
 
     // Complete the repeatedString function below.
     static long repeatedString(String s, long n) {
-        int a = 0, x = (int) n % s.length();
-        long b = 0;
-        for(int i = 0; i < s.length(); i++)
-        {
-            if(s.charAt(i) == 'a')
-            {
-                a++;
-                if(i < n)
-                    b++;
-            }
-        }
+        long a = howMuch(s, s.length()), x = n % s.length(), b = 0;
+        if(x == 0 && n < s.length())
+            b = howMuch(s, s.length());
+        else if(x > 0)
+            b = howMuch(s, x);
         return (n/s.length())*a+b;
+    }
+
+    static long howMuch(String s, long v)
+    {
+        long a = 0;
+        for(int i = 0; i < v; i++)
+            if(s.charAt(i) == 'a')
+                a++;
+        return a;
     }
 }
