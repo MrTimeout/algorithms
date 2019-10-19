c2b37b9802878e3e0fc68e07a4815dc96eb68bec
estenoesmiputonombre <49493249+estenoesmiputonombre@users.noreply.github.com>
2019-10-19 21:43:01 +0200

    Create button.js

---
+++ hackerrank/10daysOfJavascript/day8/CreateAButton/js/button.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+  document.getElementById("btn").onclick = function() {
+        document.getElementById("btn").innerHTML = parseInt(document.getElementById("btn").innerHTML) + 1;
+  }
+}
