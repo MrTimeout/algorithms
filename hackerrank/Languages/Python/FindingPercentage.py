777b5324da7ec4c915b437541dfe3beec605d9f0
estenoesmiputonombre <49493249+estenoesmiputonombre@users.noreply.github.com>
2019-10-23 22:49:01 +0200

    Create FindingPercentage.py

---
+++ hackerrank/Languages/Python/FindingPercentage.py
@@ -0,0 +1,12 @@
+from functools import reduce
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    average = 0
+    print("%.2f" % float(reduce(lambda x, y: x + y, student_marks[query_name])/3))
